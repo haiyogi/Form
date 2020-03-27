c63ca05af5c2199c6d944d6cc3f4565f28a1966b
haiyogi <56195902+haiyogi@users.noreply.github.com>
2020-03-27 20:35:24 +0530

    upload

---
+++ form.js
@@ -0,0 +1,60 @@
+var express = require('express');
+var BodyParser = require('body-parser');
+var mysql = require('mysql');
+
+var app = express();
+
+//app.set('view engine', 'pug');
+
+
+var UrlencodedParser = BodyParser.urlencoded({extended:false});
+
+var conn = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"yogesh",
+    database:"form"
+})
+
+
+app.get('/', function (req, res) {
+    res.sendFile('index.html', { root: __dirname })
+});
+
+app.get('/form', (req,res)=>{
+    conn.query('select* from user',(err,rows,feilds)=>{
+      if(!err)
+      res.send(rows);
+      else
+        console.log(err);
+    })
+  });
+
+
+app.post('/index', UrlencodedParser, (req, res) => {
+   let name=req.body.name;
+   let email=req.body.email;
+   let password=req.body.password;
+  // console.log(req.body);
+   //console.log(req.body.name);
+   //console.log(req.body.email);
+   //console.log(req.body.password);
+   var bind = [];
+        // loop in the attributes of the object and push the values into the bind array.
+        for(prop in body){
+            bind.push(req.body[prop]);
+        }
+   conn.connect(function(err){
+       if(err) throw err;
+       console.log('Connection Successful!');
+       var sql="INSERT INTO user(name,email,password) VALUES(null,'"+name+"','"+email+"','"+password+"')";
+       conn.query(sql,(err,result)=>{
+         if(err) throw err;
+         console.log("record inserted Successfully!!");
+       });
+   });
+   //res.render('index',{message:'data saved successfully'});
+res.sendFile('link.html',{ root: __dirname },{data:req.body});
+});
+
+app.listen(2500,()=>{console.log("database working on port 2500!!")});
